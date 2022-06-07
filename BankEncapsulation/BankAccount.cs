0a13fa64e5438f2898031685bfbb432de359601f
John Tuck <j.tuck1336@gmail.com>
2022-06-07 09:15:39 -0400

    Encapsulation exercise easy peasy

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 200;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double Withdraw(double amount)
+        { return balance -= amount; }
+
+        public double GetBalance()
+
+        {
+            return balance;
+
+        }
+        
+
+    }
+
+
+
+
+
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount johnsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            johnsAccount.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank you! Your balance is now {johnsAccount.GetBalance()}.");
+
+            Console.WriteLine("How much would you like to Withdraw?");
+            var amountToWithdraw = double.Parse(Console.ReadLine());
+
+            johnsAccount.Withdraw(amountToWithdraw);
+            Console.WriteLine($"Thank you! Your balance is now {johnsAccount.GetBalance()}.");
+        }
+    }
+}
